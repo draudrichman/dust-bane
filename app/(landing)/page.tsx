2f144b1361be715cee55cd0005236e6c003d2098
draudrichman <88120992+draudrichman@users.noreply.github.com>
2024-02-10 17:03:53 +0600

    fixed landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,19 +3,21 @@ import Link from "next/link";
 
 const LandingPage = () => {
     return (
-        <div>
-            Landing Page (Unprotected)
-            <div>
-                <Link href="/sign-in">
-                    <Button>
-                        Sign in
-                    </Button>
-                </Link>
-                <Link href="/sign-up">
-                    <Button>
-                        Sign up
-                    </Button>
-                </Link>
+        <div className="flex justify-center items-center h-screen bg-black">
+            <div className="text-center">
+                <h1 className="mb-8 text-white">DustBane</h1>
+                <div className="space-y-4">
+                    <Link href="/sign-in">
+                        <Button className="mr-2 bg-white text-black hover:text-white hover:bg-zinc-800">
+                            Sign in
+                        </Button>
+                    </Link>
+                    <Link href="/sign-up">
+                        <Button className="bg-white text-black hover:text-white hover:bg-zinc-800">
+                            Sign up
+                        </Button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
